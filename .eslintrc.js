e81bb8309d2d7e11897af1619b1f8917f0e8678b
leffers <m.leffers@aljex.com>
2019-02-18 11:24:25 -0700

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-multiple-empty-lines': [2, { max: 2 }]
   },
   parserOptions: {
     parser: 'babel-eslint'
